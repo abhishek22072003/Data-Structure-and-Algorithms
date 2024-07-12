22539e5083ba7ac0e7ec66a641f6d84234c11ce5
abhishek22072003 <abhi2003patil@gmail.com>
2024-07-12 19:44:20 +0530

    Initial commit

---
+++ Dynamic Programming/PrintingLCS.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class PrintingLCS{    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        while(A-->0)
+            System.out.println("Hello World");
+    }
+}   
